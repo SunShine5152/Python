7ef8a2ff88d9bc4826befce86c04fb2dff99d6d6
liyingjie <liyingjie@simcom.com>
2019-07-16 09:16:46 +0800

    add 09

---
+++ 09/while_if.py
@@ -0,0 +1,8 @@
+bingo = 'lyj is a handsome'
+answer = input('pls input a good message:')
+
+while True:
+    if answer == bingo:
+        break;
+    answer = input('sorry,input rong')
+print('Good ,right')    
